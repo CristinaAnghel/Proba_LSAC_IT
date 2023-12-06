539fb9a61758b2a07fcb82ee51057dc33c3a30dc
CristinaAnghel <cristina.anghel004@gmail.com>
2023-12-06 19:41:54 +0200

    w

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
-import DefaultComponent from "./components/Home/Home";
 import RegisterComponent from './components/RegisterPage/RegisterPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<DefaultComponent/>}></Route>
+        <Route exact path="/" element={<TestComponent/>}></Route>
         <Route path="/test" element={<TestComponent/>}></Route>
         <Route path="/registered" element={<RegisterComponent/>}></Route>
       </Routes>
